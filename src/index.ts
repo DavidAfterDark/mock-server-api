22f93b1bee2f0f80bedb08b3ce987ae4a910cf6b
David Flores <dflores@meatcode.cl>
2025-09-11 11:14:20 -0400

    agregar ruta para manejar la recuperación de contraseña

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express'
 import onboardingRouter from '@/routes/onboarding'
 import loginRouter from '@/routes/login'
+import forgotPassword from '@/routes/forgot-password'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -14,6 +15,10 @@ app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/onboarding', onboardingRouter)
 
 app.use('/login', loginRouter)
+
+app.use('/forgot-password', forgotPassword)
+
+app.use('/forgot-password-typ', forgotPassword)
 
 // Welcome route
 app.get('/', (req: Request, res: Response) => {
